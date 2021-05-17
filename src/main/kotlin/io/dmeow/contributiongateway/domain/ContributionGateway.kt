67e2e399fe270f343f557f1a83d82b611eae62a6
dmeow17 <derek.lyc1027@gmail.com>
2021-05-18 02:00:42 +0800

    use concurrent collections to avoid concurrent accesses

--- src/main/kotlin/io/dmeow/contributiongateway/domain/ContributionGateway.kt
+++ src/main/kotlin/io/dmeow/contributiongateway/domain/ContributionGateway.kt
@@ -3,13 +3,15 @@ package io.dmeow.contributiongateway.domain
 import io.dmeow.contributiongateway.domain.model.FxQuote
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
 
 @Service
 class ContributionGateway(
     private val validationService: ValidationService,
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
-    private val fxQuotesByCcyPair = mutableMapOf<String, MutableList<FxQuote>>()
+    private val fxQuotesByCcyPair: ConcurrentHashMap<String, CopyOnWriteArrayList<FxQuote>> = ConcurrentHashMap()
 
     fun findLatestQuote(ccyPair: String): FxQuote? = findFxQuotes(ccyPair)?.first()
 
@@ -19,7 +21,7 @@ class ContributionGateway(
     fun contribute(quote: FxQuote): FxQuote {
         log.info("Contributing FxQuote [${quote.ccyPair}]")
         quote.validationResult = validationService.validate(quote)
-        fxQuotesByCcyPair.putIfAbsent(quote.ccyPair, mutableListOf())
+        fxQuotesByCcyPair.putIfAbsent(quote.ccyPair, CopyOnWriteArrayList())
         fxQuotesByCcyPair.getValue(quote.ccyPair).add(quote)
         log.info("Contributed FxQuote [${quote.ccyPair}]")
         return quote
