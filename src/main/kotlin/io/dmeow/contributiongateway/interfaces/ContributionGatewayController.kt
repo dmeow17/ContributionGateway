37103c93cd875f02d25d63b02968b64b93c8f26b
dmeow17 <derek.lyc1027@gmail.com>
2021-05-18 00:53:54 +0800

    add swagger

--- src/main/kotlin/io/dmeow/contributiongateway/interfaces/ContributionGatewayController.kt
+++ src/main/kotlin/io/dmeow/contributiongateway/interfaces/ContributionGatewayController.kt
@@ -6,13 +6,19 @@ import io.dmeow.contributiongateway.interfaces.model.FxQuoteDto
 import io.dmeow.contributiongateway.interfaces.model.FxQuoteInputDto
 import io.dmeow.contributiongateway.interfaces.model.toDomain
 import io.dmeow.contributiongateway.interfaces.model.toDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDateTime
 
 @RestController
-@RequestMapping("/api/v1/contribution")
+@RequestMapping("/api/v1/quotes")
 class ContributionGatewayController(
     private val contributionGateway: ContributionGateway,
     private val nowGenerator: () -> LocalDateTime,
@@ -21,10 +27,34 @@ class ContributionGatewayController(
         const val CCY_PAIR_VARIABLE = "{ccyPair}"
     }
 
+    @Operation(
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [Content(
+                    mediaType = APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = FxQuoteDto::class),
+                )],
+            ),
+            ApiResponse(responseCode = "404", content = [Content()]),
+        ],
+    )
     @GetMapping("/$CCY_PAIR_VARIABLE")
     fun getLatestQuote(@PathVariable ccyPair: String): FxQuoteDto =
         contributionGateway.findLatestQuote(ccyPair)?.toDto() ?: throw CcyPairNotFoundException(ccyPair)
 
+    @Operation(
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [Content(
+                    mediaType = APPLICATION_JSON_VALUE,
+                    array = ArraySchema(schema = Schema(implementation = FxQuoteDto::class))
+                )],
+            ),
+            ApiResponse(responseCode = "404", content = [Content()]),
+        ],
+    )
     @GetMapping("/$CCY_PAIR_VARIABLE/all-quotes")
     fun getAllQuotes(@PathVariable ccyPair: String): List<FxQuoteDto> =
         contributionGateway.findFxQuotes(ccyPair)?.map { it.toDto() } ?: throw CcyPairNotFoundException(ccyPair)
