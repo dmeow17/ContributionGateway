b3ee3532a6517726af09fb6eba52932a06de0e8e
dmeow17 <derek.lyc1027@gmail.com>
2021-05-18 01:19:56 +0800

    add logs in services

--- src/main/kotlin/io/dmeow/contributiongateway/domain/ContributionGateway.kt
+++ src/main/kotlin/io/dmeow/contributiongateway/domain/ContributionGateway.kt
@@ -1,12 +1,14 @@
 package io.dmeow.contributiongateway.domain
 
 import io.dmeow.contributiongateway.domain.model.FxQuote
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class ContributionGateway(
     private val validationService: ValidationService,
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
     private val fxQuotesByCcyPair = mutableMapOf<String, MutableList<FxQuote>>()
 
     fun findLatestQuote(ccyPair: String): FxQuote? = findFxQuotes(ccyPair)?.first()
@@ -15,9 +17,11 @@ class ContributionGateway(
         fxQuotesByCcyPair[ccyPair]?.sortedByDescending { it.lastUpdatedTime }
 
     fun contribute(quote: FxQuote): FxQuote {
+        log.info("Contributing FxQuote [${quote.ccyPair}]")
         quote.validationResult = validationService.validate(quote)
         fxQuotesByCcyPair.putIfAbsent(quote.ccyPair, mutableListOf())
         fxQuotesByCcyPair.getValue(quote.ccyPair).add(quote)
+        log.info("Contributed FxQuote [${quote.ccyPair}]")
         return quote
     }
 }
--- src/main/kotlin/io/dmeow/contributiongateway/domain/ValidationServiceImpl.kt
+++ src/main/kotlin/io/dmeow/contributiongateway/domain/ValidationServiceImpl.kt
@@ -3,16 +3,22 @@ package io.dmeow.contributiongateway.domain
 import io.dmeow.contributiongateway.domain.model.FxQuote
 import io.dmeow.contributiongateway.domain.model.ValidationResult
 import io.dmeow.contributiongateway.domain.model.ValidationResult.Status.SUCCEEDED
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.*
 
 // Can be implemented/swapped with prod component later
 @Service
 class ValidationServiceImpl : ValidationService {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun findValidationResult(uuid: UUID): ValidationResult? = TODO()
 
-    override fun validate(quote: FxQuote): ValidationResult = ValidationResult(
-        uuid = UUID.randomUUID(),
-        status = SUCCEEDED,
-    )
+    override fun validate(quote: FxQuote): ValidationResult {
+        log.info("Completed validation for FxQuote [${quote.ccyPair}]")
+        return ValidationResult(
+            uuid = UUID.randomUUID(),
+            status = SUCCEEDED,
+        )
+    }
 }
